d1b672900e91fc58272ededf7d8c989d0437d300
lizhenping <lizhenping18@mails.ucas.ac.cn>
2025-08-28 14:18:10 +0800

    refactor: 删除了无关代码重新开始设计

--- nl2sql_pipeline/src/nl2sql_pipeline/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Data models for NL2SQL Pipeline"""
-
-from .database import *
-from .analysis import *
-from .generation import *
-from .states import *
-from .step_results import *
-from .comprehensive_result import ComprehensiveAnalysisResult
-from .pipeline_contexts import *
